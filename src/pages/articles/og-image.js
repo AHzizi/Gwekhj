c0edfd3897ee0bb6d443e964dd57b1036ef917f8
AHzizi <miftakulazizi0@gmail.com>
2023-10-24 07:48:13 +0700

    fix puppets

--- src/pages/articles/og-image.js
+++ src/pages/articles/og-image.js
@@ -23,7 +23,9 @@ export async function generateOgImage(props) {
     // file does not exists, so we create it
   }
 
-  const browser = await puppeteer.launch({ headless: true });
+
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+  // const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 630 });
   await page.goto(url, { waitUntil: 'networkidle0' });
