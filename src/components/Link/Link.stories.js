0566b9e718a5a4ec809463f0cd508e483ba88348
AHzizi <miftakulazizi0@gmail.com>
2024-05-28 10:36:38 +0700

    initial commit

--- src/components/Link/Link.stories.js
+++ src/components/Link/Link.stories.js
@@ -7,8 +7,8 @@ export default {
 
 export const Default = () => (
   <StoryContainer style={{ fontSize: 18 }}>
-    <Link href="https://hamishw.com">Primary link</Link>
-    <Link secondary href="https://hamishw.com">
+    <Link href="https://azizii.my.id">Primary link</Link>
+    <Link secondary href="azizii.my.id">
       Secondary link
     </Link>
   </StoryContainer>
