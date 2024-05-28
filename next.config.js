b1477f790ed5b568fcfc09d06254dd6c8dcbd3bb
AHzizi <miftakulazizi0@gmail.com>
2024-05-28 12:06:34 +0700

    cape gw jirr

--- next.config.js
+++ next.config.js
@@ -6,7 +6,49 @@ module.exports = {
     topLevelAwait: true,
   },
   webpack(config, { isServer }) {
-    // Your webpack configuration
+    config.module.rules.push({
+      test: /\.(glb)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/media/[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+    // Run custom scripts
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+      require('./scripts/draco');
+    }
+
+    // Import `svg` files as React components
+    config.module.rules.push({
+      test: /\.svg$/,
+      resourceQuery: { not: [/url/] },
+      use: [{ loader: '@svgr/webpack', options: { svgo: false } }],
+    });
+
+    // Import videos, models, hdrs, and fonts
+    config.module.rules.push({
+      test: /\.(mp4|hdr|glb|woff|woff2)$/i,
+      type: 'asset/resource',
+    });
+
+    // Force url import with `?url`
+    config.module.rules.push({
+      resourceQuery: /url/,
+      type: 'asset/resource',
+    });
+
+    // Import `.glsl` shaders
+    config.module.rules.push({
+      test: /\.glsl$/,
+      type: 'asset/source',
+    });
+
     return config;
   },
 };
